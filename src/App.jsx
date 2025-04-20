f9f6046a081125d200bba355723401fa2c917775
Hiral Arora <hiral.arora1406@gmail.com>
2025-04-20 00:09:20 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage";
 import VolunteerLogin from "./volunteer_login";
+import StaffLogin from "./staff_login";
 
 const App = () => {
   return (
     <Router>
       <main>
         <Routes>
-          <Route path="/" element={<VolunteerLogin />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/volunteer_login" element={<VolunteerLogin />} />
+          <Route path="/staff_login" element={<StaffLogin />} />
         </Routes>
       </main>
     </Router>
